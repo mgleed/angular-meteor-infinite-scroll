0e44258744d844a0a19a7d960bbef06f8e17cb0f
mgleed <aliigleed16@gmail.com>
2015-11-10 12:58:35 -0700

    Added filters, changed model, added formly support

--- infinite-scroll.js
+++ infinite-scroll.js
@@ -1,26 +1,61 @@
 Images = new Mongo.Collection('images');
 
 if (Meteor.isClient) {
-    angular.module('infinite-scroll').value('THROTTLE_MILLISECONDS', 500);
-
+    angular.module('infinite-scroll').value('THROTTLE_MILLISECONDS', 500);  
+    
     Meteor.startup(function() {
         angular.bootstrap(document, ['scroll-demo']);
     });
 }
 
 if (Meteor.isServer) {
+    var tmpShotBy, tmpCategory;
     if (!Images.find().count()) {
-        for (var i = 0; i < 1000; i++) {
+        for (var i = 0; i < 100; i++) {
+            switch (i%4){
+              case 0:
+                tmpShotBy = 'Steve';
+                break;
+              case 1:
+                tmpShotBy = 'Sally';
+                break;
+              case 2:
+                tmpShotBy = 'Mark';
+                break;
+              case 3:
+                tmpShotBy = 'Rachel';
+                break;
+            }
+            switch (i%3){
+              case 0:
+                tmpCategory = 'Sports';
+                break;
+              case 1:
+                tmpCategory = 'Technology';
+                break;
+              case 2:
+                tmpCategory = 'Nature';
+                break;
+            }            
             Images.insert({
-                index: i
+                index: i,
+                dateSubmitted: new Date(),
+                shotBy: tmpShotBy,
+                category: tmpCategory,
+                favorites: Math.floor((Math.random() * 100) + 1),                
             });
         }
     }
 
     Meteor.publish('images', function(opts) {
+      if (opts === undefined){
+        return Images.find({});
+      } else {
         return Images.find({}, {
-            skip: opts.skip,
-            limit: opts.limit
+          skip: opts.skip,
+          limit: opts.limit,
+          sort: opts.sort,
         });
+      };
     });
 }
--- scroll-controller.js
+++ scroll-controller.js
@@ -1,27 +1,121 @@
 if (Meteor.isClient) {
-    var module = angular.module('scroll-demo', ['angular-meteor', 'infinite-scroll']);
+    var module = angular.module('scroll-demo', ['angular-meteor', 'infinite-scroll', 'formly', 'formlyBootstrap', "ui.select", "ui.bootstrap", 'ngSanitize']);
 
-    module.controller('ngInfiniteScrollCtrl', ['$scope', '$meteor',
+    module.run(['formlyConfig', function(formlyConfig) {
+      formlyConfig.setType({
+        name: 'ui-select',
+        extends: 'select',
+        template: [
+          '<ui-select data-ng-model="model[options.key]" data-required="{{to.required}}" data-disabled="{{to.disabled}}" theme="bootstrap">',
+            '<ui-select-match placeholder="{{to.placeholder}}" data-allow-clear="true">{{$select.selected[to.labelProp]}}</ui-select-match>',
+            '<ui-select-choices data-repeat="{{to.ngOptions}}">',
+              '<div ng-bind-html="option[to.labelProp] | highlight: $select.search"></div>',
+            '</ui-select-choices>',
+          '</ui-select>'
+        ].join('')
+      });
+      formlyConfig.setType({
+        name: 'ui-select-multiple',
+        extends: 'select',
+        template: [
+          '<ui-select multiple data-ng-model="model[options.key]" data-required="{{to.required}}" data-disabled="{{to.disabled}}" theme="bootstrap">',
+            '<ui-select-match placeholder="{{to.placeholder}}">{{$item[to.labelProp]}}</ui-select-match>',
+            '<ui-select-choices data-repeat="{{to.ngOptions}}">',
+              '<div ng-bind-html="option[to.labelProp] | highlight: $select.search"></div>',
+            '</ui-select-choices>',
+          '</ui-select>'
+        ].join('')
+      });    
+    }]);    
+    
+    module.controller('ngInfiniteScrollCtrl', ['$scope', '$meteor', '$log',
         function($scope, $meteor) {
-
-            var page = 20;
-
-            $meteor.subscribe('images', {
-                skip: 0,
-                limit: 2 * page
-            });
-
-            $scope.images = $meteor.collection(function() {
-                return Images.find();
-            });
-
+            
+            var page = 6;
+            // function executed when scrolling to the bottom
             $scope.loadMore = function() {
-                var len = $scope.images.length;
-                $meteor.subscribe('images', {
-                    skip: len,
-                    limit: page
-                });
+              var len = $scope.images.length;
+              $meteor.subscribe('images', {
+                skip: len,
+                limit: page,
+                sort: { dateSubmitted: -1 }
+              });
             };
+            
+            $meteor.autorun($scope, function() {
+              $meteor.subscribe('images', {
+                skip: 0,
+                limit: 2 * page,
+                sort: { dateSubmitted: -1 }
+              }).then(function() {
+                $scope.images = $meteor.collection(function() {
+                  var query={};
+                  // if filtered by a user...
+                  if ($scope.getReactively('model.shotBy')) {
+                    query.shotBy = $scope.model.shotBy;
+                  };
+                  // if category filter(s) are selected...
+                  if ($scope.getReactively('model.category', true)) {
+                    if ($scope.model.category.length > 0){
+                      var categories = [];
+                      for (var i=0; i < $scope.model.category.length; i++){            
+                        categories.push($scope.model.category[i]);
+                      }
+                      query.category = {$in: categories};
+                    }
+                  };
+                  return Images.find(query);
+                });
+              });
+            });            
 
+            
+            // start Angular-Formly form
+            $scope.options = {};
+            $scope.model = {
+              shotBy: null,
+              category: null,
+            };  
+            
+            $scope.fields = [
+              {
+                type: 'ui-select',
+                key: 'shotBy',
+                templateOptions: {
+                  optionsAttr: 'bs-options',
+                  ngOptions: 'option[to.valueProp] as option in to.options | filter: $select.search',                  
+                  label: 'Filter by photographer',
+                  placeholder: 'Choose photographer',
+                  required: false,
+                  options:[
+                    {name: "Steve"},
+                    {name: "Rachel"},
+                    {name: "Sally"},
+                    {name: "Mark"},
+                  ],
+                  valueProp: 'name',
+                  labelProp: 'name',
+                },
+              },
+              {     
+                type: 'ui-select-multiple',
+                key: 'category',
+                templateOptions: {
+                  label: 'Filter by category',
+                  placeholder: '  Choose categories',
+                  required: false,
+                  options:[
+                    {name: "Sports"},
+                    {name: "Technology"},
+                    {name: "Nature"},
+                  ],
+                  valueProp: 'name',
+                  labelProp: 'name', 
+                  optionsAttr: 'bs-options',
+                  ngOptions: 'option[to.valueProp] as option in to.options | filter: $select.search',          
+                },      
+              },               
+            ];              
+            
         }]);
 }
