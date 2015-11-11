786f1e17224f95c6889510467a99a3f15e22413c
mgleed <aliigleed16@gmail.com>
2015-11-10 20:12:45 -0700

    added sort select, changed ngInfiniteScroll version, rewrote sub/col portion

--- infinite-scroll.js
+++ infinite-scroll.js
@@ -9,53 +9,53 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-    var tmpShotBy, tmpCategory;
-    if (!Images.find().count()) {
-        for (var i = 0; i < 100; i++) {
-            switch (i%4){
-              case 0:
-                tmpShotBy = 'Steve';
-                break;
-              case 1:
-                tmpShotBy = 'Sally';
-                break;
-              case 2:
-                tmpShotBy = 'Mark';
-                break;
-              case 3:
-                tmpShotBy = 'Rachel';
-                break;
-            }
-            switch (i%3){
-              case 0:
-                tmpCategory = 'Sports';
-                break;
-              case 1:
-                tmpCategory = 'Technology';
-                break;
-              case 2:
-                tmpCategory = 'Nature';
-                break;
-            }            
-            Images.insert({
-                index: i,
-                dateSubmitted: new Date(),
-                shotBy: tmpShotBy,
-                category: tmpCategory,
-                favorites: Math.floor((Math.random() * 100) + 1),                
-            });
-        }
+  var tmpShotBy, tmpCategory;
+  if (!Images.find().count()) {
+    for (var i = 0; i < 500; i++) {
+      switch (i%4){
+        case 0:
+          tmpShotBy = 'Steve';
+          break;
+        case 1:
+          tmpShotBy = 'Sally';
+          break;
+        case 2:
+          tmpShotBy = 'Mark';
+          break;
+        case 3:
+          tmpShotBy = 'Rachel';
+          break;
+      }
+      switch (i%3){
+        case 0:
+          tmpCategory = 'Sports';
+          break;
+        case 1:
+          tmpCategory = 'Technology';
+          break;
+        case 2:
+          tmpCategory = 'Nature';
+          break;
+      }            
+      Images.insert({
+        index: i,
+        dateSubmitted: new Date(),
+        shotBy: tmpShotBy,
+        category: tmpCategory,
+        favorites: Math.floor((Math.random() * 100) + 1),                
+      });
     }
+  }
 
-    Meteor.publish('images', function(opts) {
-      if (opts === undefined){
-        return Images.find({});
-      } else {
-        return Images.find({}, {
-          skip: opts.skip,
-          limit: opts.limit,
-          sort: opts.sort,
-        });
-      };
-    });
+  Meteor.publish('images', function(opts) {
+    if (opts === undefined){
+      return Images.find({});
+    } else {
+      return Images.find({}, {
+        skip: opts.skip,
+        limit: opts.limit,
+        sort: opts.sort,
+      });
+    };
+  });
 }
--- scroll-controller.js
+++ scroll-controller.js
@@ -30,53 +30,81 @@ if (Meteor.isClient) {
     
     module.controller('ngInfiniteScrollCtrl', ['$scope', '$meteor', '$log',
         function($scope, $meteor) {
-            
-            var page = 6;
-            // function executed when scrolling to the bottom
-            $scope.loadMore = function() {
-              var len = $scope.images.length;
-              $meteor.subscribe('images', {
-                skip: len,
-                limit: page,
-                sort: { dateSubmitted: -1 }
-              });
-            };
-            
-            $meteor.autorun($scope, function() {
-              $meteor.subscribe('images', {
-                skip: 0,
-                limit: 2 * page,
-                sort: { dateSubmitted: -1 }
-              }).then(function() {
-                $scope.images = $meteor.collection(function() {
-                  var query={};
-                  // if filtered by a user...
-                  if ($scope.getReactively('model.shotBy')) {
-                    query.shotBy = $scope.model.shotBy;
-                  };
-                  // if category filter(s) are selected...
-                  if ($scope.getReactively('model.category', true)) {
-                    if ($scope.model.category.length > 0){
-                      var categories = [];
-                      for (var i=0; i < $scope.model.category.length; i++){            
-                        categories.push($scope.model.category[i]);
-                      }
-                      query.category = {$in: categories};
-                    }
-                  };
-                  return Images.find(query);
-                });
-              });
-            });            
-
-            
-            // start Angular-Formly form
+            // some ideas from here...
+            // https://github.com/ndxbxrme/generator-angular-meteor/blob/master/generators/app/templates/bootstrap/client/main/main.controller(js).ng.js       
+          
+            // initialize Angular-Formly form //
             $scope.options = {};
             $scope.model = {
               shotBy: null,
               category: null,
+              sort: {dateSubmitted: -1},
             };  
+         
+            // infinite-scroll logic & collection subscription //
+            $scope.currentPage = 1;
+            $scope.perPage = 12;
+            $scope.orderProperty = '-1';
+            $scope.query = {};
+                      
+            $scope.images = $scope.$meteorCollection(function() {
+              query={};            
+              // if filtered by a user...
+              if ($scope.getReactively('model.shotBy')) {
+                query.shotBy = $scope.model.shotBy;
+              };
+              // if category filter(s) are selected...
+              if ($scope.getReactively('model.category', true)) {
+                if ($scope.model.category.length > 0){
+                  var categories = [];
+                  for (var i=0; i < $scope.model.category.length; i++){            
+                    categories.push($scope.model.category[i]);
+                  }
+                  query.category = {$in: categories};
+                }
+              };
+              $scope.currentPage = 1; // reset
+              return Images.find(query, { sort: $scope.getReactively('model.sort')});
+            });
             
+            $meteor.autorun($scope, function() {
+              if ($scope.getReactively('images')){
+                console.log('send filtered signal to infinite-scroll');
+                $scope.$emit('list:filtered');    
+              }
+            });
+            
+            $meteor.autorun($scope, function() {
+              console.log('inside autorun');
+              console.log('limit is');
+              console.log(parseInt($scope.getReactively('perPage')));
+              console.log('skip is');
+              console.log(parseInt(($scope.getReactively('currentPage') - 1) * $scope.getReactively('perPage')));
+              console.log('  current page is');
+              console.log($scope.getReactively('currentPage'));
+              console.log('  per page is');
+              console.log($scope.getReactively('perPage'));
+              console.log('sort is');
+              console.log($scope.getReactively('model.sort'));              
+              $scope.$meteorSubscribe('images', {
+                limit: parseInt($scope.getReactively('perPage'))*parseInt(($scope.getReactively('currentPage'))),
+                skip: 0,
+                sort: $scope.getReactively('model.sort')
+              });
+            });              
+            
+            $scope.loadMore = function() {
+              console.log('loading more');
+              $scope.currentPage += 1;
+            };         
+            
+            function setSortObj(value) {
+              var tmpObj = new Object;
+              tmpObj[value] = -1;
+              return tmpObj;
+            }                      
+            
+            // Formly fields //
             $scope.fields = [
               {
                 type: 'ui-select',
@@ -114,7 +142,26 @@ if (Meteor.isClient) {
                   optionsAttr: 'bs-options',
                   ngOptions: 'option[to.valueProp] as option in to.options | filter: $select.search',          
                 },      
-              },               
+              },   
+              {     
+                type: 'ui-select',
+                key: 'sort',
+                parsers: [setSortObj],
+                templateOptions: {
+                  label: 'Sort results',
+                  placeholder: '  Choose criteria',
+                  required: false,
+                  options:[
+                    {label: "Date Submitted", value: "dateSubmitted"},
+                    {label: "Index", value: "index"},
+                    {label: "Favorites", value: "favorites"},
+                  ],
+                  valueProp: 'value',
+                  labelProp: 'label',                   
+                  optionsAttr: 'bs-options',
+                  ngOptions: 'option[to.valueProp] as option in to.options | filter: $select.search',          
+                },      
+              },              
             ];              
             
         }]);
